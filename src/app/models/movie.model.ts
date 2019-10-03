7d2e7514c5abc7329ac07d815b779b9b55db56ed
Julien <julien.deflesselle@gmail.com>
2019-10-03 14:35:18 +0200

    API  + models/adapters (for movies)

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { Adapter } from './adapter';
+
+export class Movie {
+    constructor(
+      public id: number,
+      public title: string,
+      public originalTitle: string,
+      public posterPath: string,
+      public details: MovieDetails,
+    ) { }
+  }
+
+export class MovieDetails {
+    constructor(
+      public tagline: string,
+      public releaseDate: Date,
+      public overview: string,
+      public genres: Genre[],
+      public videoPath: string,
+    ) { }
+  }
+
+export class Genre {
+    constructor(
+        public id: number,
+        public name: string,
+    ) {}
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MovieAdapter implements Adapter<Movie> {
+
+  adapt(item: any): Movie {
+    return new Movie(
+      item.results.id,
+      item.results.title,
+      item.results.original_title,
+      item.results.poster_path,
+      null,
+    );
+  }
+}
+
+export class GenreAdapter implements Adapter<Genre[]> {
+    static adapt(genres: any): Genre[] {
+            const table: Genre[] = [];
+            for (const i of genres) {
+                table.push(new Genre(i.id, i.name));
+            }
+            return table;
+    }
+    adapt(item: any): Genre[] {
+        const table: Genre[] = [];
+        for (const i of item) {
+            table.push(new Genre(i.id, i.name));
+        }
+        return table;
+      }
+}
+
+export class MovieDetailsAdapter implements Adapter<MovieDetails> {
+
+    adapt(item: any): MovieDetails {
+      return new MovieDetails(
+        item.results.tagline,
+        item.results.release_date,
+        item.results.overview,
+        GenreAdapter.adapt(item.results.genres),
+        item.results.videoPath,
+      );
+    }
+}
--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -1,12 +1,71 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Movie, MovieAdapter, MovieDetailsAdapter, GenreAdapter, Genre } from 'src/app/models/movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviedbService {
 
-  apiKey : string = "c46a2dd15015ce4720d7557d3c908c7c"
+  private apiKey = 'c46a2dd15015ce4720d7557d3c908c7c';
 
-  constructor(public http: HttpClient) { }
+  private baseUrl = 'https://api.themoviedb.org/3';
+
+  constructor(
+    private httpClient: HttpClient,
+    private movieAdapter: MovieAdapter,
+    private moviedetailsAdapter: MovieDetailsAdapter,
+    private genreAdapter: GenreAdapter,
+    ) { }
+
+  searchMovie(searchStr: string): Observable <Movie[]> {
+    const url = `${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${searchStr}`;
+    return this.httpClient.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map(item => this.movieAdapter.adapt(item))),
+    );
+  }
+
+  getMovieDetails(movie: Movie): void {
+    const url = `${this.baseUrl}/movie/${movie.id}?api_key=${this.apiKey}`;
+    const res = this.httpClient.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map(item => this.moviedetailsAdapter.adapt(item))),
+    );
+    movie.details = res[0];
+  }
+
+  getInTheaters(): Observable <Movie[]> {
+    const url = `${this.baseUrl}/movie/now_playing?api_key=${this.apiKey}&region=US`;
+    return this.httpClient.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map(item => this.movieAdapter.adapt(item))),
+    );
+  }
+
+  getUpComingMovies(): Observable <Movie[]> {
+    const url = `${this.baseUrl}/movie/upcoming?api_key=${this.apiKey}&region=US`;
+    return this.httpClient.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map(item => this.movieAdapter.adapt(item))),
+    );
+  }
+
+  getGenres(): Observable <Genre[][]> {
+    const url = `${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}&language=en-US`;
+    return this.httpClient.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map(item => this.genreAdapter.adapt(item))),
+    );
+  }
+
+  getMoviesByGenre(genre: Genre) {
+    const url = `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&language=en-US&sort_by=revenue.desc&include_adult=false&with_genres=${genre.id}`;
+    return this.httpClient.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map(item => this.genreAdapter.adapt(item))),
+    );
+  }
 }
