3e151059057f70d55163ca59ac8c0fbd174dfa2e
Foenn <pidouxle@gmail.com>
2019-10-08 20:26:21 +0200

    Test routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TvshowListComponent } from './components/tvshow/tvshow-list/tvshow-list.component';
+import { MovieCardComponent } from './components/movie/movie-card/movie-card.component';
+import { PeopleListComponent } from './components/people/people-list/people-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'Séries',
+    component : TvshowListComponent
+  },
+  {
+    path : 'Films',
+    component : MovieCardComponent
+  },
+  {
+    path : 'Acteurs',
+    component : PeopleListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
