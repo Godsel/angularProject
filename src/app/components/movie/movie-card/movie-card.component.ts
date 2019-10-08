3cb4efa88b4571e90f83dbe161dff7977134288f
Julien <julien.deflesselle@gmail.com>
2019-10-08 20:12:07 +0200

    Exemple liste films, movie finder -> movie list -> movie card

--- src/app/components/movie/movie-card/movie-card.component.ts
+++ src/app/components/movie/movie-card/movie-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Movie, Genre } from '../../../models/movie.model';
 import { MoviedbService } from '../../../services/moviedb.service';
 import { map } from 'rxjs/operators';
@@ -10,33 +10,8 @@ import { MoviesRequest } from '../../../models/request.model';
   styleUrls: ['./movie-card.component.scss']
 })
 
-export class MovieCardComponent implements OnInit {
+export class MovieCardComponent {
 
-  movieRequest: MoviesRequest;
-  movies: Movie[];
-  braveheart: Movie;
-
-
-  genre: Genre = {id: 28, name: 'Action'};
-
-  constructor(
-    private moviedbService: MoviedbService,
-    ) {}
-
-  ngOnInit() {
-    this.moviedbService.searchMovie('braveheart').subscribe((res => {
-      this.movieRequest = res;
-      console.log(this.movieRequest);
-      this.movies = this.movieRequest.movies;
-      // console.log('scope is', this.braveheart);
-      this.braveheart = this.movies[0];
-      console.log(this.braveheart);
-    }));
-
-    this.moviedbService.getMoviesByGenre(this.genre, 2).subscribe((res => {
-      this.movieRequest = res;
-      console.log(this.movieRequest);
-    }));
+    @Input()
+    movie: Movie;
   }
-
-}
--- src/app/components/movie/movie-finder/movie-finder.component.ts
+++ src/app/components/movie/movie-finder/movie-finder.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviedbService } from '../../../services/moviedb.service';
-import { Genre } from '../../../models/movie.model';
-import { GenresRequest } from '../../../models/request.model';
+import { Genre, Movie } from '../../../models/movie.model';
+import { GenresRequest, MoviesRequest } from '../../../models/request.model';
 
 @Component({
   selector: 'app-movie-finder',
@@ -13,6 +13,10 @@ export class MovieFinderComponent implements OnInit {
 
   genresRequest: GenresRequest;
   listGenres: Genre[];
+  genre: Genre = {id: 28, name: 'Action'};
+
+  movieRequest: MoviesRequest;
+  movies: Movie[];
 
   constructor(
     private moviedbService: MoviedbService,
@@ -23,6 +27,17 @@ export class MovieFinderComponent implements OnInit {
       this.genresRequest = res;
       this.listGenres = this.genresRequest.genres;
     }));
-  }
 
+    this.moviedbService.searchMovie('braveheart').subscribe((res => {
+      this.movieRequest = res;
+      this.movies = this.movieRequest.movies;
+      console.log(this.movies);
+    }));
+
+    this.moviedbService.getMoviesByGenre(this.genre, 2).subscribe((res => {
+      this.movieRequest = res;
+      this.movies = this.movieRequest.movies;
+      console.log(this.movies);
+    }));
+  }
 }
