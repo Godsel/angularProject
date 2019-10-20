86ea3e54ac0364460fc1ae6f431dcedd09a94fde
Foenn <pidouxle@gmail.com>
2019-10-20 22:13:17 +0200

    Ajout Tvshows

---
+++ src/app/models/tvshow.model.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Adapter } from './adapter';
+
+export class Tvshow {
+    constructor(
+      public id: number,
+      public title: string,
+      public originalTitle: string,
+      public posterPath: string,
+      public score: number,
+      public releaseDate: Date,
+      public details: TvshowDetails,
+    ) { }
+
+    static adapt(item: any): Tvshow {
+      return new Tvshow(
+        item.id,
+        item.title,
+        item.original_title,
+        item.poster_path,
+        item.vote_average,
+        item.release_date,
+        null,
+      );
+    }
+  }
+
+export class Genre {
+    constructor(
+        public id: number,
+        public name: string,
+    ) {}
+
+    static adapt(item: any): Genre {
+      return new Genre(
+        item.id,
+        item.name,
+      );
+    }
+}
+
+export class TvshowDetails {
+    constructor(
+      public originalLanguage: string,
+      public tagline: string,
+      public overview: string,
+      public runtime: number,
+      public genres: Genre[],
+      public budget: number,
+      public revenue: number,
+      public videoPath: string,
+    ) { }
+
+    static adapt(item: any): TvshowDetails {
+      return new TvshowDetails(
+        item.original_language,
+        item.tagline,
+        item.overview,
+        item.runtime,
+        item.genres.map((data: any[]) => Genre.adapt(data)),
+        item.budget,
+        item.revenue,
+        item.videoPath,
+      );
+    }
+  }
+
