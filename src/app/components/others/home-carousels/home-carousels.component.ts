99ec708f61b7c6eb8074ca2810bb42d3f8b09a26
Foenn <pidouxle@gmail.com>
2019-10-21 19:42:01 +0200

    Add movies and tv shows carousel (bug with )

--- src/app/components/others/home-carousels/home-carousels.component.ts
+++ src/app/components/others/home-carousels/home-carousels.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviedbService } from '../../../services/moviedb.service';
 import { Movie } from '../../../models/movie.model';
-import { MoviesRequest } from '../../../models/request.model';
+import { MoviesRequest, TvshowRequest } from '../../../models/request.model';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { Tvshow } from 'src/app/models/tvshow.model';
 
 @Component({
   selector: 'app-home-carousels',
@@ -12,17 +14,30 @@ export class HomeCarouselsComponent implements OnInit {
 
   movieRequest: MoviesRequest;
   movies: Movie[];
+  tvshowRequest: TvshowRequest;
+  tvshows: Tvshow[];
+  page: number;
+  language: string;
 
   constructor(
     private moviedbService: MoviedbService,
-  ) { }
+    private translateService: TranslateService,
+  ) {
+      this.page = 1;
+      this.language = translateService.currentLang;
+  }
 
   ngOnInit() {
 
-    // this.moviedbService.getInTheaterMovies().subscribe((res => {
-    //   this.movieRequest = res;
-    //   this.movies = this.movieRequest.movies;
-    // }));
+    this.moviedbService.getInTheaterMovies(this.page, this.language).subscribe((res => {
+      this.movieRequest = res;
+      this.movies = this.movieRequest.movies;
+    }));
+
+    this.moviedbService.getOnAirTvShows(this.language, this.page).subscribe((res => {
+      this.tvshowRequest = res;
+      this.tvshows = this.tvshowRequest.tvshows;
+    }));
 
   }
 
